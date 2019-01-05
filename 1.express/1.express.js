b0fe9aeaf2f77b328ff6b152f3726319c9ce5e29
zhaoshaopeng <zhaoshaopeng@bjmantis.com>
2019-01-05 12:38:13 +0800

    商品

--- 1.express/1.express.js
+++ 1.express/1.express.js
@@ -2,7 +2,7 @@ let express = require('express');
 let fs = require('fs');
 let app = express();
 app.get('/', (req, res) => {
-    res.end('OK');
+    res.send('OK');
 });
 
 app.post('/userList', (req, res) => {
@@ -10,8 +10,28 @@ app.post('/userList', (req, res) => {
         if (err) {
             return console.error(err);
         }
-        console.log(data);
-        res.end(data);
+        data = JSON.parse(data.toString());
+        res.send(data.data);
+    })
+});
+
+app.post('/add', (req, res) => {
+    fs.readFile('./commodityData.json', (err, data) => {
+        if (err) {
+            return console.error(err);
+        }
+        req.on('data', function (parms) {
+            let request = JSON.parse(parms.toString());
+            data = JSON.parse(data.toString());
+            let dataList = data.data;
+            let count = data.count + 1;
+            request.id=count;
+            dataList.push(request);
+            data.count = count;
+            data.data = dataList;
+            let str = JSON.stringify(data);
+            add(str,res);
+        });
         /*let person = data.toString();//将二进制的数据转换为字符串
         person = JSON.parse(person);//将字符串转换为json对象
         person.data.push(params);//将传来的对象push进数组对象中
@@ -27,21 +47,22 @@ app.post('/userList', (req, res) => {
     })
 });
 
-app.post('/add', (req, res) => {
-    res.end('post about')
-});
-app.put('/about', (req, res) => {
-    res.end('post about')
-});
-app.delete('/about', (req, res) => {
-    res.end('post about')
-});
+add = (str,res) => {
+    fs.writeFile('./commodityData.json', str, function (err) {
+        if (err) {
+            console.error(err);
+        }
+        res.send({
+            "flag": 1,
+            "message": "新增成功"
+        });
+    })
+};
+
 
 app.all('*', (req, res) => {
     res.setHeader('content-type', 'text/html;charset=utf-8');
-    res.end([
-        {"message": "请求的路径不存在"}
-    ]);
+    res.send({"message": "请求的路径不存在"});
 });
 
 app.listen(3003);
